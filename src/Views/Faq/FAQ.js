f11b0889823da6771702e6c9c4218234faea130f
Goodness Urama <goodyboy301@gmail.com>
2022-07-07 11:47:52 +0100

    Populate faqs and other minor tweaks

--- src/Views/Faq/FAQ.js
+++ src/Views/Faq/FAQ.js
@@ -6,37 +6,43 @@ export const faqs = [
   {
     id: 1,
     question: "What is Que?",
-    answer: "Que is a netflix clone",
+    answer:
+      "Que is a netflix clone. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas nam quasi. Dolorem, molestiae nobis vero autem saepe suscipit dolorum obcaecati excepturi, qui sint possimus? Itaque quaerat suscipit quod facere.",
     shown: false,
   },
   {
     id: 2,
     question: "How much does que cost?",
-    answer: "Que cost nothing",
+    answer:
+      "Que cost nothing. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas nam quasi. Dolorem, molestiae nobis vero autem saepe suscipit dolorum obcaecati excepturi, qui sint possimus? Itaque quaerat suscipit quod facere.",
     shown: false,
   },
   {
     id: 3,
     question: "Where can i watch?",
-    answer: "You can watch on your tv amd mobile device",
+    answer:
+      "You can watch on your tv amd mobile device. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas nam quasi. Dolorem, molestiae nobis vero autem saepe suscipit dolorum obcaecati excepturi, qui sint possimus? Itaque quaerat suscipit quod facere.",
     shown: false,
   },
   {
     id: 4,
     question: "How do i cancel?",
-    answer: "You cannot cancel",
+    answer:
+      "You cannot cancel. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas nam quasi. Dolorem, molestiae nobis vero autem saepe suscipit dolorum obcaecati excepturi, qui sint possimus? Itaque quaerat suscipit quod facere.",
     shown: false,
   },
   {
     id: 5,
     question: "What can I watch on Que?",
-    answer: "Watch movies Tv Shows and season movies",
+    answer:
+      "Watch movies Tv Shows and season movies. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas nam quasi. Dolorem, molestiae nobis vero autem saepe suscipit dolorum obcaecati excepturi, qui sint possimus? Itaque quaerat suscipit quod facere.",
     shown: false,
   },
   {
     id: 6,
     question: "is Que Good For Kids?",
-    answer: "Yes Que IS GOOD FOR KIDS",
+    answer:
+      "Yes Que IS GOOD FOR KIDS. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad voluptas nam quasi. Dolorem, molestiae nobis vero autem saepe suscipit dolorum obcaecati excepturi, qui sint possimus? Itaque quaerat suscipit quod facere.",
     shown: false,
   },
 ];
@@ -53,14 +59,14 @@ const FAQ = () => {
 
   return (
     <section className={classes.grid}>
-      <h2>Frequently asked Questions</h2>
+      <h2>Frequently asked questions</h2>
       {faq}
       <p>
-        Ready to Watch?Enter your email to create or restart your membership
+        Ready to watch? Enter your email to create or restart your membership
       </p>
       <form action="">
         <input type="email" name="user_email" placeholder="Email Address" />
-        <button>Get Started &gt;</button>
+        <button>Get Started</button>
       </form>
     </section>
   );
